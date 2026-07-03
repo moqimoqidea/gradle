acea6e24c9cd44c2d92df0ca225474030bbf396f
Sergey Opivalov <sopivalov@gradle.com>
2026-07-03 10:09:46 +0300

    Shadow `Gradle` and `TaskExecutionGraph` services at the project scope by IP reporting wrappers

---
+++ platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/isolated/IsolatedProjectsInjectedServicesIntegrationTest.groovy
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.internal.cc.impl.isolated
+
+class IsolatedProjectsInjectedServicesIntegrationTest extends AbstractIsolatedProjectsIntegrationTest {
+
+    def "reports a problem on access to mutable state of `#service` injected into a task"() {
+        createDirs("a")
+        settingsFile """
+            include("a")
+        """
+        buildFile """
+            import javax.inject.Inject
+
+            abstract class MyTask extends DefaultTask {
+                @Inject
+                abstract $service getService()
+
+                @TaskAction
+                void run() {}
+            }
+
+            tasks.register("myTask", MyTask) {
+                it.service.$prohibitedInvocation
+            }
+        """
+
+        when:
+        isolatedProjectsFailsUsing mode, ":myTask"
+
+        then:
+        fixture.assertIsolatedProjectsProblems(mode) {
+            projectsConfigured(":", ":a")
+            problem("Build file 'build.gradle': line 13: $expectedProblem")
+        }
+
+        where:
+        service              | prohibitedInvocation                   | expectedProblem
+        "Gradle"             | "plugins"                              | "Project ':' cannot access Gradle.getPlugins"
+        "TaskExecutionGraph" | "whenReady { g -> g.hasTask(':a:x') }" | "Project ':' cannot access the tasks in the task graph that were created by other projects"
+
+        combined:
+        mode << ALL_MODES
+    }
+
+    def "reports a problem on access to mutable state of `#service` injected into a plugin"() {
+        createDirs("a")
+        settingsFile """
+            include("a")
+        """
+        buildFile """
+            import javax.inject.Inject
+
+            class MyPlugin implements Plugin<Project> {
+                private final $service service
+
+                @Inject
+                MyPlugin($service service) {
+                    this.service = service
+                }
+
+                void apply(Project project) {
+                    service.$prohibitedInvocation
+                }
+            }
+
+            apply plugin: MyPlugin
+        """
+
+        when:
+        isolatedProjectsFailsUsing mode, ":help"
+
+        then:
+        fixture.assertIsolatedProjectsProblems(mode) {
+            projectsConfigured(":", ":a")
+            problem("Build file 'build.gradle': line 13: $expectedProblem")
+        }
+
+        where:
+        service              | prohibitedInvocation                   | expectedProblem
+        "Gradle"             | "plugins"                              | "Project ':' cannot access Gradle.getPlugins"
+        "TaskExecutionGraph" | "whenReady { g -> g.hasTask(':a:x') }" | "Project ':' cannot access the tasks in the task graph that were created by other projects"
+
+        combined:
+        mode << ALL_MODES
+    }
+
+    def "reports a problem on access to mutable state of `#service` injected into an object created with ObjectFactory"() {
+        createDirs("a")
+        settingsFile """
+            include("a")
+        """
+        buildFile """
+            import javax.inject.Inject
+
+            abstract class MyBean {
+                @Inject
+                abstract $service getService()
+            }
+
+            objects.newInstance(MyBean).service.$prohibitedInvocation
+        """
+
+        when:
+        isolatedProjectsFailsUsing mode, "help"
+
+        then:
+        fixture.assertIsolatedProjectsProblems(mode) {
+            projectsConfigured(":", ":a")
+            problem("Build file 'build.gradle': line 9: $expectedProblem")
+        }
+
+        where:
+        service              | prohibitedInvocation                   | expectedProblem
+        "Gradle"             | "plugins"                              | "Project ':' cannot access Gradle.getPlugins"
+        "TaskExecutionGraph" | "whenReady { g -> g.hasTask(':a:x') }" | "Project ':' cannot access the tasks in the task graph that were created by other projects"
+
+        combined:
+        mode << ALL_MODES
+    }
+}
